f7bb97350cbcc1dbc6645fb25bd876b2d2454f3c
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-04-14 22:21:51 +0300

    eject

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MonacoEditor from 'react-monaco-editor';
+import BemjsonToJSX from 'bemjson-to-jsx';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            jsx: `<Button text='Hello world' />`,
+        }
+
+        this.onBemjsonChange = this.onBemjsonChange.bind(this);
+    }
+    onBemjsonChange(newValue, e) {
+        try {
+            debugger;
+            var bemjson = eval(newValue);
+            var jsx = BemjsonToJSX().process(bemjson).JSX
+            this.setState({jsx});
+        } catch (err) {}
+    }
+    render() {
+        const options = {
+             scrollBeyondLastLine: false
+        };
+        const code =
+`({
+    block: 'button',
+    text: 'Hello world'
+})`;
+        const jsx = this.state.jsx;
+        return (
+            <div className='App'>
+                <span className='App__editor bemjson'>
+                    <MonacoEditor {...{
+                        language: 'javascript',
+                        width: '50%',
+                        options: options,
+                        onChange: this.onBemjsonChange,
+                        value: code
+                    }}/>
+                </span>
+                <span className='App__editor jsx'>
+                    <MonacoEditor {...{
+                        language: 'jsx',
+                        width: '50%',
+                        options: options,
+                        value: jsx
+                    }}/>
+                </span>
+           </div>
+       );
+    }
 }
 
 export default App;
